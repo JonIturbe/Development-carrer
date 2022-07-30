3d98ea8e651e5f834391eddb6a9b4ad7cc996d98
Jon Iturbe <joniturbejob@gmail.com>
2022-07-30 14:49:04 +0200

    update

--- Udemy Curso Front-End/Componentes Basados en clases/mi-app/src/App.js
+++ Udemy Curso Front-End/Componentes Basados en clases/mi-app/src/App.js
@@ -1,1 +1,24 @@
+import { Component } from "react";
+
+class App extends Component {
+    //Definimos la propiedad de estado
+    state = {}
+        
+    //render() se ejecuta siempre que el estado del componente haya cambiado o cuando el metodo render de 
+    //un componente padre ha sido llamado, por lo que los componentes hijos tambien llaman a render()
+    render() {
+        console.log(this.state);
+        return (
+            <div>
+                <p>Hola mundo</p>
+                <button 
+                className={this.state.valor}
+                onClick={() => this.setState({ valor : 2 })}>
+                    Enviar
+                </button>
+            </div>
+        )
+    }
+}
+
 export default App;
