0b5802cd6254f855994f9648f6b2ff83a132c78f
Jon Iturbe <joniturbejob@gmail.com>
2022-07-31 18:36:49 +0200

    update

--- Udemy Curso Front-End/Proyecto - Carrito de la compra/carrito-app/src/App.js
+++ Udemy Curso Front-End/Proyecto - Carrito de la compra/carrito-app/src/App.js
@@ -13,23 +13,45 @@ state = {
     { name: "Tomate", price: 1500, img:"/productos/tomates.jpg" },
     { name: "Vainas", price: 2500, img:"/productos/vainas.jpg" },
     { name: "Lechuga", price: 500, img:"/productos/lechuga.jpg" },
-  ]
+  ],
+  carro: [],
 }
 
-  render() {
-    return(
-    <div>
-      <NavBar />
-      <Layout>
-        <Title/>
-        <Productos
-          agregarAlCarro={ () => console.log("Aun no hace nada") }
-          productos={ this.state.productos }
-        />
-      </Layout>
-    </div>
-    )
+agregarAlCarro = (producto) => {
+  const { carro } = this.state;
+
+  if(carro.find(x => x.name === producto.name)) {
+    const newCarro = carro.map(x => x.name === producto.name ? ({
+      ...x, 
+      cantidad: x.cantidad + 1
+    })
+    : x)
+    return this.setState({ carro: newCarro })
   }
+
+  return this.setState({
+    carro: this.state.carro.concat({
+      ...producto,
+      cantidad: 1,
+    })
+  })
+}
+
+render() {
+  console.log(this.state.carro);
+  return(
+  <div>
+    <NavBar carro={ this.state.carro } />
+    <Layout>
+      <Title/>
+      <Productos
+        agregarAlCarro={ this.agregarAlCarro }
+        productos={ this.state.productos }
+      />
+    </Layout>
+  </div>
+  )
+}
 }
 
 export default App;
