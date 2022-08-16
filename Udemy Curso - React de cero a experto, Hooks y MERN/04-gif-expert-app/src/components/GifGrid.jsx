9b1a25c9295a3d9a9d7ba45d2ae6bba5d2ab2774
Jon Iturbe <joniturbejob@gmail.com>
2022-08-16 17:51:40 +0200

    update

--- Udemy Curso - React de cero a experto, Hooks y MERN/04-gif-expert-app/src/components/GifGrid.jsx
+++ Udemy Curso - React de cero a experto, Hooks y MERN/04-gif-expert-app/src/components/GifGrid.jsx
@@ -1,30 +1,42 @@
-import { getGifs } from "../helpers/getGifts";
-import { useState, useEffect } from "react";
+// import { useState, useEffect } from "react";
+// import { getGifs } from "../helpers/getGifts";
+import { useFetchGifs } from "../hooks/useFetchGifs";
+import { GifItem } from "./GifItem";
 
 export const GifGrid = ({ category }) => {
 
-    const [images, setImages] = useState([]);
+    const { images, isLoading } = useFetchGifs( category );
 
-    const getImages = async () => {
-        const newImages = await getGifs( category );
-        setImages(newImages);
-    }
+    ////CODIGO SIN HOOK USEFETCHGIFS
+    // const [images, setImages] = useState([]);
+    // const getImages = async () => {
+    //     const newImages = await getGifs( category );
+    //     setImages(newImages);
+    // }
 
-    useEffect( () => {
-        getImages();
-        // getGifs( category )
-        // .then( newImages => setImages(newImages) );
-    }, [  ] );
+    // useEffect( () => {
+    //     getImages();
+    //     // getGifs( category )
+    //     // .then( newImages => setImages(newImages) );
+    // }, [] );
 
     return(
         <>
             <h3>{ category }</h3>
-            <ol>
-                { images.map( ( { id, title } ) => (
-                            <li key={ id }>{ title }</li> 
+            {
+                isLoading && ( <h1>Cargando...</h1> )
+            }
+            
+            <div className="card-grid">
+                { images.map( ( img ) => (
+                            <GifItem 
+                                key={ img.id }
+                                // title={ img.title } url={ img.url }
+                                { ...img }
+                            /> 
                     ))
                 }
-            </ol>
+            </div>
         </>
     );
 }
